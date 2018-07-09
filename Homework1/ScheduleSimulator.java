c21bd4125873277c4d8f44c0a07cb25174abc882
ndobesh <31423950+ndobesh@users.noreply.github.com>
2018-07-09 17:38:59 -0500

    Initial setup of Homework1 | Put in import from file section

---
+++ Homework1/ScheduleSimulator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Scheduler {
+    public static void main( String [] args) {
+
+
+        /*Read from file code from:
+        https://stackoverflow.com/questions/19844649/java-read-file-and-store-text-in-an-array */
+        String toBeQueue = "";
+
+        Scanner inFile1 = new Scanner(new File("KeyWestTemp.txt")).useDelimiter(",\\s*");
+    }
+}
